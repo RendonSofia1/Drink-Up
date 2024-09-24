af56840f74b274a82aec628463b28a362cf6be87
RendonSofia1 <rendonsofi03@gmail.com>
2024-09-24 17:45:59 -0600

    page menu
    incomplete

--- bar/src/app/pages/user/menu/menu.page.ts
+++ bar/src/app/pages/user/menu/menu.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonItem, IonCardContent, IonList, IonLabel, IonCardTitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonItem, IonCardContent, IonList, IonLabel, IonCardTitle, IonCardSubtitle, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ToolbarComponent } from "../../../components/toolbar/toolbar.component";
 import { TabsComponent } from "../../../components/tabs/tabs.component";
+import { addIcons } from 'ionicons';
+import { cartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonLabel, IonList, IonCardContent, IonItem, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ToolbarComponent, TabsComponent]
+  imports: [IonIcon, IonButton, IonCardSubtitle, IonCardTitle, IonLabel, IonList, IonCardContent, IonItem, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ToolbarComponent, TabsComponent]
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({cartOutline});
+  }
 
   ngOnInit() {
   }
