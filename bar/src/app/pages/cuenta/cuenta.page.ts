b184b3ca01825aba25117b9499d602bb3f44f4e6
RendonSofia1 <rendonsofi03@gmail.com>
2024-09-24 19:27:33 -0600

    cuenta page

--- bar/src/app/pages/cuenta/cuenta.page.ts
+++ bar/src/app/pages/cuenta/cuenta.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonCardSubtitle, IonLabel } from '@ionic/angular/standalone';
 import { ToolbarComponent } from 'src/app/components/toolbar/toolbar.component';
 
 @Component({
@@ -9,7 +9,7 @@ import { ToolbarComponent } from 'src/app/components/toolbar/toolbar.component';
   templateUrl: './cuenta.page.html',
   styleUrls: ['./cuenta.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ToolbarComponent]
+  imports: [IonLabel, IonCardSubtitle, IonButton, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ToolbarComponent]
 })
 export class CuentaPage implements OnInit {
 
