cb7d257d4ebd4c348db3cf7904452110f9f758d7
RendonSofia1 <rendonsofi03@gmail.com>
2024-10-08 01:57:31 -0600

    modal editar

--- bar/src/app/pages/admin/drinks/drinks.page.ts
+++ bar/src/app/pages/admin/drinks/drinks.page.ts
@@ -1,21 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { ToolbarComponent } from "../../../components/toolbar/toolbar.component";
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonCard,
+  IonAvatar,
+  IonItem,
+  IonButton,
+  IonLabel,
+  IonThumbnail,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonModal,
+  IonInput,
+  IonButtons,
+  IonText,
+  IonGrid,
+  IonRow,
+  IonCol,
+  ModalController,
+} from '@ionic/angular/standalone';
+import { ToolbarComponent } from '../../../components/toolbar/toolbar.component';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-drinks',
   templateUrl: './drinks.page.html',
   styleUrls: ['./drinks.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ToolbarComponent]
+  imports: [
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonText,
+    IonButtons,
+    IonInput,
+    IonModal,
+    IonIcon,
+    IonFabButton,
+    IonFab,
+    IonLabel,
+    IonButton,
+    IonItem,
+    IonAvatar,
+    IonCard,
+    IonSearchbar,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    ToolbarComponent,
+    IonThumbnail,
+  ],
 })
 export class DrinksPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private modalController: ModalController) {
+    addIcons({ add });
   }
 
+  dismissModal() {
+    this.modalController.dismiss();
+  }
+
+
+  ngOnInit() {}
 }
